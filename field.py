efa4f462e0eb054e96ac19301a12b4f12038a5f8
Cfnatic7 <michalpasicki1@gmail.com>
2022-05-27 20:50:51 +0200

    Zreformatowałem klasę field

--- field.py
+++ field.py
@@ -1,10 +1,36 @@
 #klasa Field przechowująca nazwę objektu oraz jego nazwę
+from const import *
+
 class Field():
-    def __init__(self,name,x,y):
-        self.name=name
+    def __init__(self,name: str, x: int, y: int):
+        self.__name=name
         self.__fieldType=NONE
-        self.x=x
-        self.y=y
+        self.__x=x
+        self.__y=y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, newX: int):
+        self.__x = newX
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, newY: int):
+        self.__y = newY
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, newName: str):
+        self.__name = newName
 
     @property
     def fieldType(self):
@@ -18,8 +44,3 @@ class Field():
             raise ValueError
 
         self.__fieldType = newType
-
-class coordinates():
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
