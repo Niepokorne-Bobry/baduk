bac0da2066217662329c0f9e10d5ab7cabe57f49
TheWeebsSlayer <66646362+TheWeebsSlayer@users.noreply.github.com>
2022-05-27 16:00:27 +0200

    Dodanie klasy field w której jest nazwa objektu(pola) oraz jego wspolrzednych a także czy pole jest zajęte przez ktoregos z graczy

--- field.py
+++ field.py
@@ -1,23 +1,25 @@
-from const import *
-
-
-class Field:
-
-    def __init__(self):
-        self.__fieldType = NONE
+#klasa Field przechowująca nazwę objektu oraz jego nazwę
+class Field():
+    def __init__(self,name,x,y):
+        self.name=name
+        self.__fieldType=NONE
+        self.x=x
+        self.y=y
 
     @property
     def fieldType(self):
         return self.__fieldType
-    
+
     @fieldType.setter
-    def fieldType(self,newType):
-        if (newType != NONE 
-        and newType != PLAYER_1_STONE 
-        and newType != PLAYER_2_STONE):
+    def fieldType(self, newType):
+        if (newType != NONE
+                and newType != PLAYER_1_STONE
+                and newType != PLAYER_2_STONE):
             raise ValueError
-        
+
         self.__fieldType = newType
 
-
-
+class coordinates():
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
