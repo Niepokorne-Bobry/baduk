300e246f5a70e2c299b90e6e0a6d24686b170390
TheWeebsSlayer <66646362+TheWeebsSlayer@users.noreply.github.com>
2022-06-03 20:10:39 +0200

    Dodanie w mainie przyciskow, w klasi board rysowanie kolek, musze jeszcze zrobic metode do naprowadzania kolek na pole

--- Board.py
+++ Board.py
@@ -1,30 +1,52 @@
-"""
-size=9  liczba linii 9 pionowo 9 poziomo
-SIZE=(size*90)+90  zmienna do rozmiaru boku czyli dlugosci kwadtatów wynaczonych przez linie gdy mamy np 3 linie to dziela one bok na 4 czesci wiec 9 linii to 10 kwadratów
-line_color='#D4AF37'  kolor linii ciemne złoto
-LINE_WIDTH=16      szerokosc linii
-"""
-class Board():
-    def __init__(self,size):
-        self.size=size
-# wpisywanie do listy pól (objektów) na których można klasc kamienie
-    def set_fields(self,x,y):
-            zmienna = 1
-            self.__logical_board = []
+from field import Field
+from constants import line_color, LINE_WIDTH, LINE_LENGTH
+import pygame
+
+
+class Board:
+    def __init__(self, size: int):
+        if size != 9 and size != 13 and size != 19:
+            raise ValueError("incorrect size of the board")
+        self.__size = size
+        zmienna = 1
+        self.__fields = []
+        tempRow = []
+        for i in range(self.__size):
+            for j in range(self.__size):
+                name = 'object' + str(zmienna)
+                tempRow.append(Field(name, LINE_LENGTH + (j * LINE_LENGTH), LINE_LENGTH + (i * LINE_LENGTH)))
+                zmienna += 1
+            self.__fields.append(tempRow)
             tempRow = []
-            for i in range(self.size):
-                for j in range(self.size):
-                    name = 'object' + str(zmienna)
-                    tempRow.append(Field(name,90+(j*90),90+(i*90)))
-                    zmienna += 1
-                self.__logical_board.append(tempRow)
-                tempRow = []
-#funkcja do rysowania linii
-    tempCoordinates=[]
-    def draw_lines(self):
-        for i in range(size):
-            x = i * 90
-            #poziome linie
-            pygame.draw.line(screen, line_color, [0, 90 + x], [SIZE, 90 + x], LINE_WIDTH)
-            #pionowe linie
-            pygame.draw.line(screen, line_color, [90 + x, 0], [90 + x, SIZE], LINE_WIDTH)
+
+    # funkcja do rysowania linii
+    tempCoordinates = []
+
+    def draw_lines(self, screen, SIZE):
+        for i in range(self.__size):
+            x = i * LINE_LENGTH
+            # poziome linie
+            pygame.draw.line(screen, line_color, [0, LINE_LENGTH + x], [SIZE, LINE_LENGTH + x], LINE_WIDTH)
+            # pionowe linie
+            pygame.draw.line(screen, line_color, [LINE_LENGTH + x, 0], [LINE_LENGTH + x, SIZE], LINE_WIDTH)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def fields(self):
+        return self.__fields
+    #rysowanie kamienia o okreslonym kolorze (color) na wspolrzednych xi y
+    def drawStone(self,field,color,x,y):
+        self.__width=30
+        self.__x=x
+        self.__y=y
+        pygame.draw.circle(field, color, [x, y], self.__width)
+    #"----#--------" rysowanie kolka bez wypelnienia
+    def rightclickdraw(self,field,color,x,y):
+        self.__width=30
+        self.__circlewidth=5
+        self.__x = x
+        self.__y = y
+        pygame.draw.circle(field, color, [x, y], self.__width,self.__circlewidth)
--- field.py
+++ field.py
@@ -1,10 +1,37 @@
-#klasa Field przechowująca nazwę objektu oraz jego nazwę
+# klasa Field przechowująca nazwę objektu oraz jego nazwę
+from field_types import *
+
+
 class Field():
-    def __init__(self,name,x,y):
-        self.name=name
-        self.__fieldType=NONE
-        self.x=x
-        self.y=y
+    def __init__(self, name: str, x: int, y: int):
+        self.__name = name
+        self.__fieldType = FieldTypes.NONE
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, newX: int):
+        self.__x = newX
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, newY: int):
+        self.__y = newY
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, newName: str):
+        self.__name = newName
 
     @property
     def fieldType(self):
@@ -12,14 +39,9 @@ class Field():
 
     @fieldType.setter
     def fieldType(self, newType):
-        if (newType != NONE
-                and newType != PLAYER_1_STONE
-                and newType != PLAYER_2_STONE):
+        if (newType != FieldTypes.NONE
+                and newType != FieldTypes.PLAYER_1_STONE
+                and newType != FieldTypes.PLAYER_2_STONE):
             raise ValueError
 
         self.__fieldType = newType
-
-class coordinates():
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
