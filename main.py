ef6986c1303e6cf2b08f391379f2d7141f111d0e
TheWeebsSlayer <66646362+TheWeebsSlayer@users.noreply.github.com>
2022-06-03 20:59:24 +0200

    Dodanie rysowania oraz przyciskiw mainie

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pygame
 from game_class import Game
-from constants import size, SIZE
+from constants import size, SIZE, player_one_color
 # Obrazy
-# bg_img = pygame.image.load("smoczek_tlo.png")
-# bg_img = pygame.transform.scale(bg_img, (SIZE, SIZE))
+bg_img = pygame.image.load("smoczek_tlo.png")
+bg_img = pygame.transform.scale(bg_img, (SIZE, SIZE))
 BG_COLOR = (50, 170, 156)
 pygame.init()
 screen = pygame.display.set_mode((SIZE, SIZE))
 pygame.display.set_caption('Baduk')
-# screen.blit(bg_img, (0, 0))
-screen.fill(BG_COLOR)
+screen.blit(bg_img, (0, 0))
+#screen.fill(BG_COLOR)
 game = Game(size)
 game.board.draw_lines(screen, SIZE)
 pygame.display.flip()
@@ -18,3 +18,20 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                #lewy przycisk
+                game.board.drawStone(screen, player_one_color, 30, 30)
+            elif event.button == 3:
+                #prawy przycisk
+                game.board.rightclickdraw(screen, player_one_color, 60, 60)
+            pygame.display.flip()
+            """
+            elif event.button == 2:
+                #srodkowy przycisk
+            elif event.button == 4:
+                print("mouse wheel up")
+            elif event.button == 5:
+                print("mouse wheel down")
+            """
+
