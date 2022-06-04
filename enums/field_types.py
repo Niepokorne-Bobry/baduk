db1a67514071785a783c39101a40f2c3c1c270a5
TheWeebsSlayer <66646362+TheWeebsSlayer@users.noreply.github.com>
2022-06-04 22:01:25 +0200

    Poprawa klikania zmiany w const

--- enums/field_types.py
+++
@@ -1,8 +0,0 @@
-# Field types
-from enum import Enum
-
-
-class FieldTypes(Enum):
-    NONE = 0
-    PLAYER_1_STONE = 1
-    PLAYER_2_STONE = 2
--- enums/move_types.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class MoveType(Enum):
-    MOVE_OK = 0
-    MOVE_CAPTURE = 1
-    MOVE_ILLEGAL = 2
