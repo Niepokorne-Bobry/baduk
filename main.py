3512feba9bbf9d0ca67538c0f9e8ef529d34f492
TheWeebsSlayer <66646362+TheWeebsSlayer@users.noreply.github.com>
2022-06-06 01:34:31 +0200

    Menu w formie beta

--- main.py
+++ main.py
@@ -1,38 +1,76 @@
-import pygame
+import pygame, sys
 from game_class import Game
 from constants import size, SIZE, PLAYER_ONE_COLOR, PLAYER_TWO_COLOR
-# Obrazy
-bg_img = pygame.image.load("smoczek_tlo.png")
-bg_img = pygame.transform.scale(bg_img, (SIZE, SIZE))
-BG_COLOR = (50, 170, 156)
-pygame.init()
-screen = pygame.display.set_mode((SIZE, SIZE))
-pygame.display.set_caption('Baduk')
-screen.blit(bg_img, (0, 0))
-#screen.fill(BG_COLOR)
-game = Game(size)
-game.board.draw_lines(screen, SIZE)
-pygame.display.flip()
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
-            if event.button == 1:
-                #lewy przycisk
-                game.board.drawStone(screen, PLAYER_ONE_COLOR, x, y)
-            elif event.button == 3:
-                #prawy przycisk
-                game.board.rightclickdraw(screen, PLAYER_ONE_COLOR, x, y)
-            pygame.display.flip()
-            """
-            elif event.button == 2:
-                #srodkowy przycisk
-            elif event.button == 4:
-                print("mouse wheel up")
-            elif event.button == 5:
-                print("mouse wheel down")
-            """
 
+mainClock = pygame.time.Clock()
+from pygame.locals import *
+
+pygame.init()
+pygame.display.set_caption('Baduk')
+screen = pygame.display.set_mode((500, 500), 0, 32)
+
+font = pygame.font.SysFont(None, 20)
+
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+
+click = False
+#MENU
+def main_menu():
+    while True:
+        screen.fill((0, 0, 0))
+        draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
+        mx, my = pygame.mouse.get_pos()
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+        pygame.display.update()
+        mainClock.tick(60)
+def game():
+    bg_img = pygame.image.load("smoczek_tlo.png")
+    bg_img = pygame.transform.scale(bg_img, (SIZE, SIZE))
+    BG_COLOR = (50, 170, 156)
+    pygame.init()
+    screen = pygame.display.set_mode((SIZE, SIZE))
+    pygame.display.set_caption('Baduk')
+    screen.blit(bg_img, (0, 0))
+    # screen.fill(BG_COLOR)
+    game = Game(size)
+    game.board.draw_lines(screen, SIZE)
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                if event.button == 1:
+                    # lewy przycisk
+                    game.board.drawStone(screen, PLAYER_ONE_COLOR, x, y)
+                elif event.button == 3:
+                    # prawy przycisk
+                    game.board.rightclickdraw(screen, PLAYER_ONE_COLOR, x, y)
+                pygame.display.flip()
+main_menu()
