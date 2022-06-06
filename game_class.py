b45780fbc0746b712a25bb01aaf547f82162a3ca
Cfnatic7 <michalpasicki1@gmail.com>
2022-06-06 03:55:51 +0200

    Dałem kilka poprawek, gierka działa solidnie

--- game_class.py
+++ game_class.py
@@ -2,19 +2,21 @@ from field import *
 from enums.move_types import *
 from board import Board
 from player import Player
+from constants import PLAYER_ONE_COLOR, PLAYER_TWO_COLOR
+
 
 class Game:
     def __init__(self, size: int):
-        self.player1 = Player(FieldTypes.PLAYER_1_STONE,self)
-        self.player2 = Player(FieldTypes.PLAYER_2_STONE,self)
+        self.__players = [Player(FieldTypes.PLAYER_1_STONE, self, PLAYER_ONE_COLOR), Player(FieldTypes.PLAYER_2_STONE, self, PLAYER_TWO_COLOR)]
         self.__board: Board = Board(size)
+        self.__active_player = 0
 
     @property
     def board(self):
         return self.__board
 
     def validateMove(self, xCoord: int, yCoord: int):
-        if self.__board.fields[yCoord][xCoord].fieldType != FieldTypes.NONE:
+        if self.__board.fields[xCoord][yCoord].fieldType != FieldTypes.NONE:
             return MoveType.MOVE_ILLEGAL
         else:
             return MoveType.MOVE_OK
@@ -23,3 +25,9 @@ class Game:
         for i in range(self.board.size):
             for j in range(self.board.size):
                 self.board.fields[i][j].wasChecked = False
+
+    def playerToggle(self):
+        self.__active_player = 1 if self.__active_player == 0 else 0
+
+    def getActivePlayer(self):
+        return self.__players[self.__active_player]
