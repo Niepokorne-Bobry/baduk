98e3f55f2c09e317cee6f594d5363d172a66ebb6
Cfnatic7 <michalpasicki1@gmail.com>
2022-06-15 22:36:29 +0200

    Dobra powinno wszystko działać

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ __root = tk.Tk()
 FPS = 30  # frames per second
 
 size = 9  # liczba linii 9 pionowo 9 poziomo
-SIZE = __root.winfo_screenheight() * 0.7  # zmienna do rozmiaru boku czyli dlugosci kwadtatów wynaczonych przez linie gdy mamy np 3 linie to dziela one bok na 4 czesci wiec 9 linii to 10 kwadratów
+SIZE = floor(__root.winfo_screenheight() * 0.7)  # zmienna do rozmiaru boku czyli dlugosci kwadtatów wynaczonych przez linie gdy mamy np 3 linie to dziela one bok na 4 czesci wiec 9 linii to 10 kwadratów
 
 LINE_LENGTH = SIZE / 12
 FIELD_OFFSET = (SIZE - (LINE_LENGTH * (size - 1))) / 4
@@ -14,16 +14,16 @@ line_color = '#D4AF37'  # kolor linii ciemne złoto
 LINE_WIDTH = floor(LINE_LENGTH / 6)  # szerokosc linii
 PLAYER_ONE_COLOR = (0, 0, 0)
 PLAYER_TWO_COLOR = (255, 255, 255)
-CIRCLE_RADIUS = LINE_LENGTH * 0.4
+CIRCLE_RADIUS = floor(LINE_LENGTH * 0.4)
 CIRCLE_WIDTH = 5
 
-CLICKABLE_FIELD_SIZE = LINE_LENGTH / 7  # ilosc pixeli w kazdym kierunku od wspolrzednych punktu - zasięg przypisania kamienia do pola
+CLICKABLE_FIELD_SIZE = floor(LINE_LENGTH / 7)  # ilosc pixeli w kazdym kierunku od wspolrzednych punktu - zasięg przypisania kamienia do pola
 
-HUD_SIZE = SIZE / 12
+HUD_SIZE = floor(SIZE / 12)
 PLAYER_BUTTONS_SECTION_SIZE = 100
 # Menu size
 MENU_WIDTH = 800
-MENU_HEIGHT = SIZE
+MENU_HEIGHT = floor(SIZE)
 
 BUTTON_SECTION_POS_Y = SIZE + HUD_SIZE
 
